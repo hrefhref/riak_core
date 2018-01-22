ae1b61151b1503114464866432a6eb6ec7a9bda1
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2018-01-22 13:43:27 -0500

    Disable/enable vnode partitioning for partisan.

--- src/riak_core_vnode.erl
+++ src/riak_core_vnode.erl
@@ -213,7 +213,14 @@ send_command_after(Time, Request) ->
 
 init([Mod, Index, InitialInactivityTimeout, Forward]) ->
     process_flag(trap_exit, true),
-    put(index, Index),
+
+    case partisan_config:get(vnode_partitioning, false) of
+        true ->
+            put(index, Index);
+        false ->
+            ok
+    end, 
+
     State = #state{index=Index, mod=Mod, forward=Forward,
                    inactivity_timeout=InitialInactivityTimeout},
     %% Check if parallel disabled, if enabled (default)
